14db5faa54a8eb8dce40769f0f2adeddc0c4ee67
Italo Pessoa <italo.pessoa@hotmail.com>
2024-09-12 19:09:28 -0300

    ci: configure terraform pipeline for api (#139)
    * ci: config docker build step to collect image name
    
    * use DOCKER_METADATA_OUTPUT_TAGS
    
    * add api_docker_image to vars file
    
    * please, just work
    
    * lets see if this works
    
    * go
    
    * read from output
    
    * create api.auto.tfvars
    
    * this is the right one
    
    * ci: add terraform on the pipeline
    
    * update tf variables
    
    * fix terraform plan
    
    * add secrets to env
    
    * TEST
    
    * extract secrets
    
    * just echo
    
    * such a boring meeting
    
    * gogogo
    
    * maybe this works
    
    * carai, nao vi o echo
    
    * I wanna see it
    
    * boraaaa
    
    * small mistakes
    
    * extract content on tfvars
    
    * test
    
    * vars
    
    * show me the file
    
    * a new approach
    
    * nao e possivel jesus
    
    * vamo ve se nao tem agora
    
    * anything should work
    
    * tudo empty
    
    * mysql
    
    * i don't understand
    
    * x
    
    * rds not x
    
    * puta que pariu, nao acredito que era so isso
    
    * fix tf variables
    
    * remove rds variable
    
    * fix issuer
    
    * fix docker build step

---
+++ tf/variables.tf
@@ -0,0 +1,76 @@
+variable "rds_cluster_identifier" {
+  type    = string
+  default = "techchallenge-mysql-default"
+}
+
+variable "profile" {
+  description = "AWS profile name"
+  type        = string
+  default     = "default"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "eks_cluster_name" {
+  type    = string
+  default = "quixada"
+}
+
+variable "apgw_name" {
+  type    = string
+  default = "authenticator"
+}
+
+variable "mercadopago_webhook_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "mercadopago_accesstoken" {
+  type      = string
+  sensitive = true
+}
+
+variable "jwt_signing_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "jwt_issuer" {
+  type      = string
+  sensitive = false
+  default   = "https://localhost:7004"
+}
+
+variable "jwt_aud" {
+  type      = string
+  sensitive = false
+  default   = "https://localhost:7004"
+}
+
+variable "api_docker_image" {
+  type    = string
+  default = "ghcr.io/soat-fiap/fiap.techchallenge.bytemeburger/api:sha-b83177c"
+}
+
+
+variable "internal_elb_name" {
+  type    = string
+  default = "api_internal_elb"
+}
+
+variable "db_user" {
+  type      = string
+  sensitive = true
+  default   = "db_user"
+}
+
+variable "db_pwd" {
+  type      = string
+  sensitive = true
+  default   = "db_password"
+}
