0e6d24f7011bf781e04120422c5af1336595b69c
Italo Pessoa <italo.pessoa@hotmail.com>
2024-09-19 13:22:42 -0300

    create sqs for bmb events

--- src/FIAP.TechChallenge.ByteMeBurger.Publisher.Sqs/SqsSettings.cs
+++ src/FIAP.TechChallenge.ByteMeBurger.Publisher.Sqs/SqsSettings.cs
@@ -1,10 +1,32 @@
 namespace FIAP.TechChallenge.ByteMeBurger.Publisher.Sqs;
 
+/// <summary>
+/// Sqs settings
+/// </summary>
 public class SqsSettings
 {
+    /// <summary>
+    /// SQS Queue Name
+    /// </summary>
     public string QueueName { get; set; } = null!;
 
+    /// <summary>
+    /// Enabled
+    /// </summary>
     public bool Enabled { get; set; } = false;
 
+    /// <summary>
+    /// AWS Region
+    /// </summary>
     public string Region { get; set; } = null!;
+
+    /// <summary>
+    /// AWS Secret Id
+    /// </summary>
+    public string ClientSecret { get; set; } = null!;
+
+    /// <summary>
+    /// AWS Client Id
+    /// </summary>
+    public string ClientId { get; set; } = null!;
 }
